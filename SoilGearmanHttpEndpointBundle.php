4064aacbf78d57dc7280db3256028dd1a8d4102c
George Novik <grgnvk@gmail.com>
2015-02-05 15:05:32 +0300

    First commit

---
+++ SoilGearmanHttpEndpointBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 5.2.15
+ * Time: 13.54
+ */
+
+namespace Soil\QueueHttpEndpointBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SoilQueueHttpEndpointBundle extends Bundle  {
+
+} 
