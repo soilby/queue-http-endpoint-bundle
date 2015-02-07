90ed4ad14c06678ef8d0a9a2649b77c934b021eb
George Novik <grgnvk@gmail.com>
2015-02-07 22:02:58 +0300

    Add http endpoint for gearman queue

---
+++ Controller/EndpointController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Soil\QueueHttpEndpointBundle\Controller;
+
+use Soilby\EventComponent\Service\GearmanClient;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 5.2.15
+ * Time: 15.14
+ */
+
+class EndpointController {
+
+    /**
+     * @var GearmanClient
+     */
+    protected $gearmanClient;
+
+    protected $queueStream;
+
+    public function __construct($gearmanClient, $queueStream) {
+        $this->gearmanClient = $gearmanClient;
+        $this->queueStream = $queueStream;
+    }
+
+
+    public function putJobAction(Request $request) {
+        try {
+            $data = $request->get('data');
+
+            $this->gearmanClient->send($this->queueStream, $data);
+
+            $response = new JsonResponse([
+                'success' => true
+            ]);
+        }
+        catch(\Exception $e)    {
+            $response = new JsonResponse([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+
+        return $response;
+    }
+} 
