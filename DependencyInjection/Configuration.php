b5200335a2d3eae13d62185fd95c73e65bdf9857
George Novik <grgnvk@gmail.com>
2015-02-09 15:46:47 +0300

    Disable queue stream config resolving.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,17 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('soil_event_processor');
-
-        $rootNode
-            ->children()
-                ->scalarNode('queue_stream_name')->defaultValue('soil_event')->end()
-            ->end();
-
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
--- DependencyInjection/SoilQueueHttpEndpointExtension.php
+++ DependencyInjection/SoilQueueHttpEndpointExtension.php
@@ -19,12 +19,10 @@ class SoilQueueHttpEndpointExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+//        $configuration = new Configuration();
+//        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-
-        $container->setParameter('queue_stream_name', $config['queue_stream_name']);
     }
 }
